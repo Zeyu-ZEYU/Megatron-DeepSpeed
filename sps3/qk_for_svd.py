b6375a638b48de7d2691f3c0cc40ba673070e5c4
pcl-projects <qxc4fh@virginia.edu>
2024-05-09 15:16:56 -0400

    added zeroc and some new figs; keep sps3 for previous version

--- sps3/qk_for_svd.py
+++ sps3/qk_for_svd.py
@@ -11,14 +11,34 @@ with open("../../qk_for_svd.pkl", "rb") as f:
 
 svd_usvh_v_list = [[None for _ in range(16)] for _ in range(24)]
 
+layer = 12
+head = 10
+tsr = qk_list[layer][head]
+tsr = torch.concat(tsr, dim=0)
 
-for layer in range(24):
-    for head in range(16):
-        tsr = qk_list[layer][head]
-        tsr = torch.concat(tsr, dim=0)
-        U, S, V = torch.linalg.svd(tsr, full_matrices=False)
-        svd_usvh_v_list[layer][head] = V.transpose(0, 1)
+one = tsr[:512, :]
+U, S, RT = torch.linalg.svd(one, full_matrices=False)
+R = RT.transpose(0, 1)
+
+for i in range(7):
+    sec = tsr[512 * (i + 1) : 512 * (i + 2), :]
+    U, S, RT = torch.linalg.svd(sec, full_matrices=False)
+    R2 = RT.transpose(0, 1)
+    diff = R2 - R
+    # diffj = torch.abs(diff)
+    # Rj = torch.abs(R)
+    print(R2)
+    # print(R)
+    # print(ratio)
 
 
-with open("./svd_usvh_v.pkl", "wb") as f:
-    pickle.dump(svd_usvh_v_list, f)
+# for layer in range(24):
+#     for head in range(16):
+#         tsr = qk_list[layer][head]
+#         tsr = torch.concat(tsr, dim=0)
+#         U, S, V = torch.linalg.svd(tsr, full_matrices=False)
+#         svd_usvh_v_list[layer][head] = V.transpose(0, 1)
+
+
+# with open("./svd_usvh_v.pkl", "wb") as f:
+#     pickle.dump(svd_usvh_v_list, f)
